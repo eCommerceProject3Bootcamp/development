a593321a5a66df4d50130c05c5295640a8f52439
Kayle7777 <jessewebb2@gmail.com>
2018-10-20 14:27:26 -0500

    Tear out mongoose, add sequelize -- next step is to configure config.json, set up schema and models and a seed script for users // testing purposes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,10 +15,12 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// Connect to the Sequelize DB
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
