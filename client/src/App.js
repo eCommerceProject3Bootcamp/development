bad7c0387e01ff1d744aede1b938993bbca59fc4
Kayle7777 <jessewebb2@gmail.com>
2018-10-21 14:23:22 -0500

    Added react router, thank god it's simple enough

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Dashboard from './components/materials/testDashboard/Dashboard';
-
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Dashboard />
-            </div>
-        );
-    }
-}
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Test from './components/test';
+const App = () => (
+    <Router>
+        {/* Default Header thing can go here, before the Switch */}
+        <div>
+            <Switch>
+                <Route exact path="/admin/dashboard" component={Dashboard} />
+                <Route exact path="/test" component={Test} />
+                <Route component={Dashboard} />
+            </Switch>
+        </div>
+    </Router>
+);
 
 export default App;
