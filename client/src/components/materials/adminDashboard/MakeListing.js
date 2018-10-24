fe861f100242fbcceb1089faf71193ddbbc6efa4
Kayle7777 <jessewebb2@gmail.com>
2018-10-24 16:27:25 -0500

    move to home dt

--- client/src/components/materials/adminDashboard/MakeListing.js
+++ client/src/components/materials/adminDashboard/MakeListing.js
@@ -1,84 +1,91 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {AttachFile as AttachFileIcon, Menu as MenuIcon, Add as AddIcon, Done as DoneIcon} from "@material-ui/icons";
-import {
-    Divider,
-    Typography,
-    Grid,
-    MenuItem,
-    TextField,
-    FormGroup,
-    Button,
-    FormLabel
-} from "@material-ui/core";
-
-import classNames from "classnames";
-import {withStyles} from "@material-ui/core/styles";
-import {styles} from "./makeListingStyles";
+import { AttachFile as AttachFileIcon } from "@material-ui/icons";
+import { Divider, Grid, TextField, Button } from "@material-ui/core";
+// import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import { styles } from "./makeListingStyles";
+import axios from 'axios';
 
 class MakeListing extends Component {
-    state = {
-        thumbnails: [],
-        pictures: [],
-        description: "",
-        name: ""
-    };
-
-    handleTextChange = name => event => {
-        this.setState({[name]: event.target.value});
-    };
-
-    handleFileUpload = (selectorFiles: FileList) => {
-		console.log(selectorFiles)
+	state = {
+		thumbnails: [],
+		pictures: [],
+		description: "",
+		name: ""
 	};
 
-    formSubmit = event => {
-        event.preventDefault();
-        console.log(event.target);
-    };
+	handleTextChange = name => event => {
+		this.setState( { [ name ]: event.target.value } );
+	};
 
-    _chooseFileClick() {
-        this._inputLabel.click();
-    }
+	handleFileUpload = ( selectorFiles : FileList ) => {
+		console.log( selectorFiles )
+		axios.post( "/api/imageUpload", selectorFiles );
+	};
 
-    render() {
-        const {classes} = this.props;
-        return (<Grid container="container" className={classes.root} spacing={16}>
-            <Grid item="item" xs={12}>
-                <Grid container="container" justify="center" spacing={20}>
-                    <form onSubmit={e => this.formSubmit(e)}>
-                        <FormGroup>
-                            <Grid item="item" xs={4}>
-                                <TextField onChange={() => this.handleTextChange("name")} required="required" id="listing-name" label="Name" className={classes.textField} margin="normal"/>
-                            </Grid>
-                            <Grid item="item" xs="xs">
-                                <TextField onChange={() => this.handleTextChange("description")} multiline="multiline" rowsMax="4" required="required" id="listing-description" label="Description" className={classes.textField} margin="normal"/> {/* <TextField required="required" id="listing-tags" label="Required" defaultValue="Hello World" className={classes.textField} margin="normal"/> */}
-                            </Grid>
-                            <Divider/>
-                            <Grid item="item" xs="xs">
-                                <input accept="image/*" className={classes.input} style={{
-                                        display: "none"
-                                    }} id="file-upload" multiple="multiple" type="file" onChange={(e) => this.handleFileUpload(e.target.files)}/>
-                                <label htmlFor="file-upload" ref={x => this._inputLabel = x}>
-                                    <Button onClick={() => this._chooseFileClick()}>
-                                        <AddIcon/>
-                                        Add Images
-                                    </Button>
-                                </label>
-                            </Grid>
-                            <Button variant="contained" type="submit" className={classes.button}>
-                                Submit
-                            </Button>
-                        </FormGroup>
-                    </form>
-                </Grid>
-            </Grid>
-        </Grid>);
-    }
+	formSubmit = event => {
+		event.preventDefault();
+		// console.log( event.target.files );
+	};
+
+	_chooseFileClick() {
+		setTimeout( () => {
+			this._inputLabel.click();
+		}, 200 )
+	}
+
+	render() {
+		const { classes } = this.props;
+		return ( <Grid container={true} className={classes.root} spacing={16}>
+			<Grid item={true} xs={12}>
+				<Grid container={true} justify="center" spacing={20}>
+					<form onSubmit={e => this.formSubmit( e )}>
+						<Grid item={true} xs={4}>
+							<TextField onChange={() => this.handleTextChange( "name" )} required="required" id="listing-name" label="Name" className={classes.textField} margin="normal"/>
+						</Grid>
+						<Grid item={true} xs={12}>
+							<TextField
+								onChange={() => this.handleTextChange( "description" )}
+								multiline={true}
+								rowsMax="4"
+								required={true}
+								id="listing-description"
+								label="Description"
+								className={classes.textField}
+								margin="normal"/> {/* <TextField required="required" id="listing-tags" label="Required" defaultValue="Hello World" className={classes.textField} margin="normal"/> */}
+						</Grid>
+						<Divider/>
+						<Grid item={true} xs={12}>
+							<input
+								className={classes.input}
+								style={{
+									display: "none"
+								}}
+								id="file-upload"
+								multiple={true}
+								type="file"
+								accept="image/*"
+								onChange={( e ) => this.handleFileUpload( e.target.files )}/>
+							<label htmlFor="file-upload" ref={x => this._inputLabel = x}>
+								<Button onClick={() => this._chooseFileClick()}>
+									<AttachFileIcon/>
+									Add Images
+								</Button>
+							</label>
+						</Grid>
+						<Button variant="contained" type="submit" className={classes.button}>
+							Submit
+						</Button>
+					</form>
+				</Grid>
+			</Grid>
+		</Grid> );
+	}
 }
 
 MakeListing.propTypes = {
-    classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(MakeListing);
+export default withStyles( styles )( MakeListing );
--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -1,84 +1,71 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
+// ********************************************************************************* api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// ********************************************************************************* Dependencies ============================================================= Requiring our Todo model
+const db = require( "../../models" );
 
-// Dependencies
-// =============================================================
+// Routes =============================================================
+module.exports = function( app ) {
 
-// Requiring our Todo model
-const db = require("../../models");
+	// route for image upload
+	app.post( "/imageUpload/", ( req, res ) => {
+		console.log( res );
+	} )
 
-// Routes
-// =============================================================
-module.exports = function(app) {
+	// GET route for getting all of the Items
+	app.get( "/items/", function( req, res ) {
+		db.Item.findAll( {} ).then( function( dbItem ) {
+			res.json( dbItem );
+		} );
+	} );
 
-  // GET route for getting all of the Items
-  app.get("/api/Items/", function(req, res) {
-    db.Item.findAll({})
-      .then(function(dbItem) {
-        res.json(dbItem);
-      });
-  });
+	// Get route for returning Items of a specific category
+	app.get( "/items/category/:category", function( req, res ) {
+		db.Item.findAll( {
+			where: {
+				category: req.params.category
+			}
+		} ).then( function( dbItem ) {
+			res.json( dbItem );
+		} );
+	} );
 
-  // Get route for returning Items of a specific category
-  app.get("/api/Items/category/:category", function(req, res) {
-    db.Item.findAll({
-      where: {
-        category: req.params.category
-      }
-    })
-      .then(function(dbItem) {
-        res.json(dbItem);
-      });
-  });
+	// Get route for retrieving a single Item
+	app.get( "/items/:id", function( req, res ) {
+		db.Item.findOne( {
+			where: {
+				id: req.params.id
+			}
+		} ).then( function( dbItem ) {
+			res.json( dbItem );
+		} );
+	} );
 
-  // Get route for retrieving a single Item
-  app.get("/api/Items/:id", function(req, res) {
-    db.Item.findOne({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbItem) {
-        res.json(dbItem);
-      });
-  });
+	// Item route for saving a new Item
+	app.Item( "/items", function( req, res ) {
+		console.log( req.body );
+		db.Item.create( { title: req.body.title, body: req.body.body, category: req.body.category } ).then( function( dbItem ) {
+			res.json( dbItem );
+		} );
+	} );
 
-  // Item route for saving a new Item
-  app.Item("/api/Items", function(req, res) {
-    console.log(req.body);
-    db.Item.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    })
-      .then(function(dbItem) {
-        res.json(dbItem);
-      });
-  });
+	// DELETE route for deleting Items
+	app.delete( "/items/:id", function( req, res ) {
+		db.Item.destroy( {
+			where: {
+				id: req.params.id
+			}
+		} ).then( function( dbItem ) {
+			res.json( dbItem );
+		} );
+	} );
 
-  // DELETE route for deleting Items
-  app.delete("/api/Items/:id", function(req, res) {
-    db.Item.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbItem) {
-        res.json(dbItem);
-      });
-  });
-
-  // PUT route for updating Items
-  app.put("/api/Items", function(req, res) {
-    db.Item.update(req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      })
-      .then(function(dbItem) {
-        res.json(dbItem);
-      });
-  });
+	// PUT route for updating Items
+	app.put( "/items", function( req, res ) {
+		db.Item.update( req.body, {
+			where: {
+				id: req.body.id
+			}
+		} ).then( function( dbItem ) {
+			res.json( dbItem );
+		} );
+	} );
 };
