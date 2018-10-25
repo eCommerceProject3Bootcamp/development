935ba7e8dc02ca699ea6ae8434b29101a0605e96
Kayle7777 <jessewebb2@gmail.com>
2018-10-25 09:15:43 -0500

    Break changes break at work that is

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
 let options = {
-	json: { limit: '50mb', extended: true },
-	urlencoded: { limit: '50mb', extended: true, parameterLimit: '1000000' },
+    json: { limit: '50mb', extended: true },
+    urlencoded: { limit: '50mb', extended: true, parameterLimit: '1000000' },
 };
 
 // Define middleware here
@@ -22,17 +22,17 @@ app.use(routes);
 // app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
 }
 
 // Add routes
 
 // Connect to the Sequelize DB Start the API server WITH db
-db.sequelize.sync().then(function() {
-	app.listen(PORT, function() {
-		console.log('App listening on PORT ' + PORT);
-	});
-});
+// db.sequelize.sync().then(function() {
+// 	app.listen(PORT, function() {
+// 		console.log('App listening on PORT ' + PORT);
+// 	});
+// });
 // server WITHOUT db
-
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 // app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
