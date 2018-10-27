e7c0d8242b4c348ebd1159c1c1208cbfd160a984
Kayle7777 <jessewebb2@gmail.com>
2018-10-27 17:48:51 -0500

    Final fix.

--- client/src/components/materials/adminDashboard/MakeListing.js
+++ client/src/components/materials/adminDashboard/MakeListing.js
@@ -82,7 +82,6 @@ class MakeListing extends Component {
         try {
             // In production, I'm not sure what this has to be changed to, if anything
             const response = await axios.post('http://localhost:3001/api/products/upload', this.state);
-            console.log(response);
             if (response.data) {
                 this.resetState();
             }
@@ -115,8 +114,7 @@ class MakeListing extends Component {
                                 button
                                 selected={index === this.state.selectedThumbnail}
                                 onClick={event => this.handleListItemClick(event, index)}
-                                key={`RNG_${Math.floor(Math.random() * 10000)}`}
-                            >
+                                key={`RNG_${Math.floor(Math.random() * 10000)}`}>
                                 <Thumbnail classes={classes} image={image.data} />
                             </ListItem>
                         ))}
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,7 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 const uploadRoutes = require('./uploadRoutes');
 
 // Matches with /api
 router.use('/products', uploadRoutes);
-=======
-const imgRoutes = require('./imgRoutes');
-
-// Image stuff routes
-router.use('/images', imgRoutes);
->>>>>>> master
 
 module.exports = router;
