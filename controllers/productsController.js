b7d8f525ed7cfa5334d231bef4eca31df9c45ac5
kayle7777 <jessewebb2@gmail.com>
2018-10-30 08:52:22 -0500

    This is some tough stuff. Blobs and files and man...

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,26 +1,20 @@
 const db = require('../models');
-const blobUtil = require('blob-util');
+const b = require('based-blob');
 const fs = require('fs');
 const path = require('path');
-global.atob = require('atob');
+// global.atob = require('atob');
 
 module.exports = {
     upload: function(req, res) {
         const { name, description, pictures } = req.body;
         convertedPics = pictures.map(eachBlob => {
-            return blobUtil.base64StringToBlob(eachBlob.data, eachBlob.type);
+            return b.toBlob(eachBlob.data, eachBlob.type);
         });
         for (let i; i < convertedPics.length; i++) {
             if (!i === convertedPics.length - 1) {
-                fs.appendFileSync(
-                    path.join(__dirname, '../scripts/pictures.json'),
-                    `{"type": ${pictures[i].type}, "pictures": [${convertedPics[i]}]},`
-                );
+                fs.appendFileSync(path.join(__dirname, '../scripts/pictures.json'), `{"type": ${pictures[i].type}, "pictures": [${convertedPics[i]}]},`);
             } else {
-                fs.appendFileSync(
-                    path.join(__dirname, '../scripts/pictures.json'),
-                    `{"type": ${pictures[i].type}, "pictures": [${convertedPics[i]}]}]`
-                );
+                fs.appendFileSync(path.join(__dirname, '../scripts/pictures.json'), `{"type": ${pictures[i].type}, "pictures": [${convertedPics[i]}]}]`);
             }
         }
         db.Listing.create({
