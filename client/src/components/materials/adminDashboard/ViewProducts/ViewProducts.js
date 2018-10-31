5ee984650f52ac17d2c6e6aff6e74d8b944887e9
Kayle7777 <jessewebb2@gmail.com>
2018-10-31 16:52:30 -0500

    Moving from work laptop to home

--- client/src/components/materials/adminDashboard/ViewProducts/ViewProducts.js
+++ client/src/components/materials/adminDashboard/ViewProducts/ViewProducts.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
+import axios from 'axios';
 
 class ViewProducts extends Component {
-    state = {};
+    state = {
+        products: [],
+    };
+    componentDidMount() {
+        axios.get('http://localhost:3001/api/products/').then(data => this.setState({ products: data }));
+    }
     render() {
         return <div>Hello World</div>;
     }
