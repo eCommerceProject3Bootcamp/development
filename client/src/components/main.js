695b582c3686036cd24f509e9feb66c09d323aa5
Willeo <willeo.smith@outlook.com>
2018-11-03 12:16:17 -0500

    updated Nav bar component link

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import AppBar from '@material-ui/core/AppBar';
+// import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
@@ -13,6 +13,7 @@ import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Nav from '../components/Nav';
 
 const styles = theme => ({
     appBar: {
@@ -70,14 +71,7 @@ function Album(props) {
     return (
         <React.Fragment>
             <CssBaseline />
-            <AppBar position="static" className={classes.appBar}>
-                <Toolbar>
-                    <CameraIcon className={classes.icon} />
-                    <Typography variant="h6" color="inherit" noWrap>
-                        Album layout
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+            <Nav />
             <main>
                 {/* Hero unit */}
                 <div className={classes.heroUnit}>
